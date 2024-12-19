08cc44814ef42238d20f45004620eed716367719
Alex <115743030+Vynohradskyi@users.noreply.github.com>
2024-12-20 00:52:56 +0100

    zad2ov.js

--- zad2ov.js
+++ zad2ov.js
@@ -1,2 +1,9 @@
-document.write("Ostatnia modyfikacja strony: ".fontcolor("#C0C0C0").bold().fontsize(6) + "<s>");
-document.write(document.lastModified);
+// Tworzenie elementu i ustawianie jego stylu za pomocą CSS
+const text = document.createElement("div");
+text.style.color = "#C0C0C0";
+text.style.fontWeight = "bold";
+text.style.fontSize = "6rem";
+text.innerHTML = `Ostatnia modyfikacja strony: <s>${document.lastModified}</s>`;
+
+// Dodanie elementu do strony
+document.body.appendChild(text);
